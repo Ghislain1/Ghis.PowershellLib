9f769c9ea9d88b077ed690b25e991408728b5595
Ghis <Gzeleu@protonmail.com>
2019-09-16 21:26:57 +0200

    Modified read me fiel

--- Ghis.PowershellLib.ConsoleDemo/Program.cs
+++ Ghis.PowershellLib.ConsoleDemo/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace Ghis.PowershellLib.ConsoleDemo
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static async Task Main(string[] args)
         {
+            IPowershellService powershellService = new PowershellService();
+
+            var scruipt = $"Get-ChildItem  -Path ./ -Recurse -File -Force";
+            await powershellService.RunCommand(scruipt, filePath =>
+              {
+                  Console.WriteLine(filePath);
+              });
+
             Console.WriteLine("Hello World!");
+            Console.ReadKey();
         }
     }
 }
